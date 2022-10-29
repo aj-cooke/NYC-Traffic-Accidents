b2c1e1630a15424facb64f4804bf05917c3feeee
aj-cooke <62916946+aj-cooke@users.noreply.github.com>
2022-10-28 23:34:36 -0400

    added lat and long

--- Get_Zips.py
+++ Get_Zips.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 24 11:27:01 2022
-
-@author: Andy
-"""
-
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
@@ -12,13 +5,15 @@ from time import sleep
 
 segments = pd.read_csv('Documents/segments.csv')
 
-def get_zips(df, street, c1, c2):
+def get_zip_lat_long(df, street, c1, c2):
     df['int1'] = df[street] + "+%26+" + df[c1]
     df['int2'] = df[street] + "+%26+" + df[c2]
     df['int1'] = df['int1'].replace(' ', '+', regex = True)
     df['int2'] = df['int2'].replace(' ', '+', regex = True)
     df['zip1'] = 'X'
     df['zip2'] = 'X'
+    df['ll1'] = 'X'
+    df['ll2'] = 'X'
     
     for i in df.index:
         url = 'https://www.google.com/maps/place/' + df['int1'][i] + '+NY'
@@ -28,6 +23,8 @@ def get_zips(df, street, c1, c2):
             return df
         z = soup.find(", NY")
         df['zip1'][i] = soup[z + 5: z + 10]
+        ll = soup.find(";ll=")
+        df['ll1'][i] = soup[ll + 4: ll + 24]
         sleep(1.1)
         
         url = 'https://www.google.com/maps/place/' + df['int2'][i] + '+NY'
@@ -37,15 +34,18 @@ def get_zips(df, street, c1, c2):
             return df
         z = soup.find(", NY")
         df['zip2'][i] = soup[z + 5: z + 10]
+        ll = soup.find(";ll=")
+        df['ll2'][i] = soup[ll + 4: ll + 24]
         sleep(1.1)
         
         print(df['id'][i], df[street][i])
         
+    # make the actual lat and long columns
+    df[['lat1', 'long1']] = df['ll1'].str.split(',', expand=True)
+    df[['lat2', 'long2']] = df['ll1'].str.split(',', expand=True)
+    
     return df
 
-zips2 = get_zips(segments[segments['id'] >= 2390], 'Roadway.Name', 'From', 'To')
-zips2 = zips2[zips2['id'] < 2390]
-zips = pd.concat([zips, zips2], axis = 0)
-
-# can also join lat and long
-# draw lat long line on map, heat map to show traffic volume
+zips2 = get_zip_lat_long(segments[segments['id'] < 2], 'Roadway.Name', 'From', 'To')
+#zips2 = zips2[zips2['id'] < 2390]
+#zips = pd.concat([zips, zips2], axis = 0)
